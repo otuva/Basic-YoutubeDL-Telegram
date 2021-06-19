9c93f2f116d4ac8695c7476e010e227c06643155
Onur Alp Akin <67955086+otuva@users.noreply.github.com>
2021-06-19 14:03:55 +0300

    update config

--- config.py
+++ config.py
@@ -1,2 +1,25 @@
-settings = {"bot_token": "BOT-TOKEN"}
-users_allowed = [USER1, USER2, USER3]
+settings = {"bot_token": "<BOT_TOKEN>",
+            "language": "tr",  # tr/en
+            "yt_archive_file": "archive_files/vids"
+            }
+
+article_settings = {"enabled": True,
+                    "path": "articles",
+                    "User-Agent": "python-req",
+                    "archive-file": "archive_files/pages"
+                    }
+
+youtube_audio = {"download_archive": settings["yt_archive_file"],
+                 "format": "bestaudio",
+                 "restrictfilenames": "true",
+                 "outtmpl": "%(playlist)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s"
+                 }
+
+youtube_video = {"download_archive": settings["yt_archive_file"],
+                 "format": "best",
+                 "restrictfilenames": "true",
+                 "outtmpl": "%(playlist)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s",
+                 "cookiefile": "cookies.txt"
+                 }
+
+users_allowed = [USER2, USER1, USER0]
